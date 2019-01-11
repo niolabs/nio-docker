2fc82a8310b0f8bb5e834994bea8ff456cb4d123
mattdodge <matt@n.io>
2019-01-11 13:20:47 -0800

    Move block installation to entrypoint script

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Install block dependencies
+# Install block dependencies first
 cd /nio/project
 # Find any requirements.txt files at the first level of a block and install it
 find blocks -name requirements.txt -maxdepth 2 | xargs -I {} pip install -r {}
 
-# Run nio
-exec nio_run -r /nio/project
+# Run the arguments to this script as an executable
+exec "$@"
